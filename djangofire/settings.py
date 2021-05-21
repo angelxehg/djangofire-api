245f9295d564b6c55cb2b6c129a22cac879e5ac1
Angel Hurtado <sudo@angelxehg.com>
2021-05-21 16:53:36 -0500

    Pedir que usuario este autenticado con DRF

--- djangofire/settings.py
+++ djangofire/settings.py
@@ -104,6 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
